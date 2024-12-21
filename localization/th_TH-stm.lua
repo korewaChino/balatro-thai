2124b7a96bb47638f8f051017bdf45d3c8152214
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2024-12-22 00:12:10 +0700

    modloader strings

--- localization/th_TH-stm.lua
+++ localization/th_TH-stm.lua
@@ -9,66 +9,66 @@ return {
             },
             load_failure_d = {
                 text = {
-                    'Missing {C:attention}dependencies!',
+                    'ขาด{C:attention}ดีเพนเดนซี!',
                     '#1#',
                 }
             },
             load_failure_c = {
                 text = {
-                    'Unresolved {C:attention}conflicts!',
+                    'ม็อด{C:attention}ชนกัน{}!',
                     '#1#'
                 }
             },
             load_failure_d_c = {
                 text = {
-                    'Missing {C:attention}dependencies!',
+                    'ขาด {C:attention}ดีเพนเดนซี!',
                     '#1#',
-                    'Unresolved {C:attention}conflicts!',
+                    'ม็อด{C:attention}ชนกัน!',
                     '#2#'
                 }
             },
             load_failure_o = {
                 text = {
-                    '{C:attention}Outdated!{} Steamodded',
-                    'versions {C:money}0.9.8{} and below',
-                    'are no longer supported.'
+                    'Steamodded {C:attention}เก่าเกินไป!{}',
+                    'เวอร์ชัน {C:money}0.9.8{} และต่ำกว่า',
+                    'จะไม่ได้รับการสนับสนุนแล้ว'
                 }
             },
             load_failure_i = {
                 text = {
-                    '{C:attention}Incompatible!{} Needs version',
-                    '#1# of Steamodded,',
-                    'but #2# is installed.'
+                    '{C:attention}เข้ากันไม่ได้!{} ต้องการเวอร์ชัน',
+                    '#1# ของ Steamodded,',
+                    'แต่ลง #2# ไว้'
                 }
             },
             load_failure_p = {
                 text = {
-                    '{C:attention}Prefix Conflict!{}',
-                    'This mod\'s prefix is',
-                    'the same as another mod\'s.',
+                    '{C:attention}พรีฟิกซ์ขัดกัน!{}',
+                    'พรีฟิกซ์ของม็อด',
+                    'เหมือนกับม็อด',
                     '({C:attention}#1#{})'
                 }
             },
             load_failure_m = {
                 text = {
-                    '{C:attention}Main File Not Found!{}',
-                    'This mod\'s main file',
-                    'could not be found.',
+                    '{C:attention}หาไฟล์หลักไม่เจอ!{}',
+                    'หาไฟล์หลักของม็อดนี้',
+                    'ไม่เจอ',
                     '({C:attention}#1#{})'
                 }
             },
             load_disabled = {
                 text = {
-                    'This mod has been',
-                    '{C:attention}disabled!{}'
+                    'ม็อดได้ถูก',
+                    '{C:attention}ปิดการใช้งาน{}'
                 }
             }
         },
         Edition = {
             e_negative_playing_card = {
-                name = "Negative",
+                name = "เนกาทีฟ",
                 text = {
-                    "{C:dark_edition}+#1#{} hand size"
+                    "ขนาดมือ {C:dark_edition}+#1#{} ใบ"
                 },
             },
         }
@@ -78,50 +78,50 @@ return {
             hidden_achievement = "???",
         },
         achievement_descriptions = {
-            hidden_achievement = "Play more to find out!",
+            hidden_achievement = "เล่นไปเรื่อยๆ เพื่อหา!",
         },
         dictionary = {
-            b_mods = 'Mods',
-            b_mods_cap = 'MODS',
-            b_modded_version = 'Modded Version!',
+            b_mods = 'ม็อด',
+            b_mods_cap = 'ม็อด',
+            b_modded_version = 'ลงม็อด!',
             b_steamodded = 'Steamodded',
-            b_credits = 'Credits',
-            b_open_mods_dir = 'Open Mods directory',
-            b_no_mods = 'No mods have been detected...',
-            b_mod_list = 'List of Activated Mods',
-            b_mod_loader = 'Mod Loader',
-            b_developed_by = 'developed by ',
-            b_rewrite_by = 'Rewrite by ',
-            b_github_project = 'Github Project',
-            b_github_bugs_1 = 'You can report bugs and',
-            b_github_bugs_2 = 'submit contributions there.',
-            b_disable_mod_badges = 'Disable Mod Badges',
-            b_author = 'Author',
-            b_authors = 'Authors',
-            b_unknown = 'Unknown',
-            b_lovely_mod = '(Lovely Mod) ',
-            b_by = ' By: ',
-			b_config = "Config",
-			b_additions = 'Additions',
-      		b_stickers = 'Stickers',
-			b_achievements = "Achievements",
-      		b_applies_stakes_1 = 'Applies ',
+            b_credits = 'เครดิต',
+            b_open_mods_dir = 'เปิดโฟลเดอร์ม็อด',
+            b_no_mods = 'ตรวจไม่พบม็อดใดๆ...',
+            b_mod_list = 'รายชื่อม็อดที่เปิดอยู่',
+            b_mod_loader = 'ตัวโหลดม็อด',
+            b_developed_by = 'พัฒนาโดย',
+            b_rewrite_by = 'เขียนใหม่โดย by ',
+            b_github_project = 'โปรเจกต์บน GitHub',
+            b_github_bugs_1 = 'คุณสามารถรายงานข้อผิดพลาด',
+            b_github_bugs_2 = 'และช่วยพัฒนาได้ที่นั่น',
+            b_disable_mod_badges = 'ปิดเครื่องหมายม็อด',
+            b_author = 'เจ้าของ',
+            b_authors = 'เจ้าของ',
+            b_unknown = 'ไม่รู้',
+            b_lovely_mod = '(ม็อด Lovely) ',
+            b_by = ' โดย: ',
+			b_config = "ตั้งค่า",
+			b_additions = 'สิ่งที่เพิ่ม',
+      		b_stickers = 'สติกเกอร์',
+			b_achievements = "ความสำเร็จ",
+      		b_applies_stakes_1 = 'ให้เอฟเฟค',
 			b_applies_stakes_2 = '',
-			b_graphics_mipmap_level = "Mipmap level",
-			b_browse = 'Browse',
-			b_search_prompt = 'Search for mods',
-			b_search_button = 'Search',
-            b_seeded_unlocks = 'Seeded unlocks',
-            b_seeded_unlocks_info = 'Enable unlocks and discoveries in seeded runs',
+			b_graphics_mipmap_level = "ระดับมิปแมป",
+			b_browse = 'เปิดดู',
+			b_search_prompt = 'ค้นหาม็อด',
+			b_search_button = 'ค้นหา',
+            b_seeded_unlocks = 'ปลดล็อคของในรันใส่ซีด',
+            b_seeded_unlocks_info = 'อนุญาตให้ปลดล็อคของและค้นพบสิ่งต่างๆในรันใส่ซีด',
             ml_achievement_settings = {
-                'Disabled',
-                'Enabled',
-                'Bypass Restrictions'
+                'ปิด',
+                'เปิด',
+                'ข้ามข้อจำกัด'
             }
 		},
 		v_dictionary = {
-			c_types = '#1# Types',
-			cashout_hidden = '...and #1# more',
+			c_types = '#1# ประเภท',
+			cashout_hidden = '...และมีอีก #1#',
 		},
 	}
 }
